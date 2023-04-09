ac9dcc0dbff4b2bff907c7354ed25d4550dd600f
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-04-09 23:01:11 +0300

    if else modification

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -55,18 +55,15 @@ const options = {
       flatpicker.destroy();
     }
 
-    if (selectedDates[0] - Date.now() <= 0) {
-      // console.log('Date in Past');
-      Notify.failure('Please choose a date in the future');
-      refs.buttonStartTimer.disabled = true;
-      return;
-    }
-
     if (selectedDates[0] - Date.now() > 0) {
       //   console.log('Date in Future');
       refs.buttonStartTimer.disabled = false;
       Notify.success('You selected a date in the future');
       refs.buttonStartTimer.addEventListener('click', onStartTimer);
+    } else {
+      Notify.failure('Please choose a date in the future');
+      refs.buttonStartTimer.disabled = true;
+      return;
     }
   },
 };
